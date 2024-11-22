96749610760eaa4253182ede9c47d95a9b217773
Wálisson Andrey <85573798+PennyWASS@users.noreply.github.com>
2024-11-22 12:37:15 -0300

    Adicionando mais funcionalidades para a página principal e usuários comuns

--- Banco de dados/logar.php
+++ Banco de dados/logar.php
@@ -31,6 +31,10 @@
         $pegarId = mysqli_query($conexao, "SELECT id FROM usuarios WHERE email = '$email' AND senha = '$senha'");
         $id = mysqli_fetch_assoc($pegarId);
         $_SESSION["usuario_id_admin"] = $id['id'];
+        //Pegando o nome do usuário
+        $pegarNome = mysqli_query($conexao, "SELECT nome FROM usuarios WHERE email = '$email' AND senha = '$senha'");
+        $nome = mysqli_fetch_assoc($pegarNome);
+        $_SESSION["nome"] = $nome['nome'];
         header('location: ../Paginas/index-admin.php');
         exit();
     }
@@ -43,6 +47,10 @@
         $pegarId = mysqli_query($conexao, "SELECT id FROM usuarios WHERE email = '$email' AND senha = '$senha'");
         $id = mysqli_fetch_assoc($pegarId);
         $_SESSION["usuario_id"] = $id['id'];
+        //Pegando o nome do usuário
+        $pegarNome = mysqli_query($conexao, "SELECT nome FROM usuarios WHERE email = '$email' AND senha = '$senha'");
+        $nome = mysqli_fetch_assoc($pegarNome);
+        $_SESSION["nome"] = $nome['nome'];
         // Redirecionamento para a página principal
         header('location: ../Paginas/index.php');
         exit();
--- Banco de dados/mostrar-post-comentario.php
+++ Banco de dados/mostrar-post-comentario.php
@@ -2,7 +2,8 @@
     // Conexão com o banco de dados
     include("conectarDB.php");
     session_start();
-
+    $usuario_id = $_SESSION['usuario_id'];
+    $nome_usuario = $_SESSION['nome'];
     // Criando a tabela de comentários caso ela não exista
     $verificaTabelaComentarios = mysqli_query($conexao, "SHOW TABLES LIKE 'comentarios'"); 
     if(mysqli_num_rows($verificaTabelaComentarios) == 0){
@@ -24,17 +25,43 @@
         while ($tabela = mysqli_fetch_array($dados)) {
             // Mostrando os posts
             echo "<h4>". $tabela["titulo"]. "</h4>";
+            if($usuario_id == $tabela['usuario_id']){
+                echo("Criado por: <b>". $nome_usuario  ."</b> <br>");
+            }
             if($tabela["imagem"] != ""){
                 echo "<img width='300px' alt='Imagem' src='".$tabela["imagem"]."'>" . "<br>";
             }
             echo $tabela["conteudo"] . "<br>";
+
+            // Verificando se o post foi feito por aquele usuario e exibindo o botão de excluir post
+            if($usuario_id == $tabela['usuario_id']){
+                echo "<form action='../Banco de dados/mostrar-post-comentario.php' method='POST'>
+                <input type='submit' name='excluirPost' value='Excluir Post'> <br> <br> <!--excluir post-->
+                <input type='hidden' name='post_id' value='".$tabela["id"]."'>
+                
+                </form>";
+            }
+
             echo "<h3>Comentários:</h3>";
             
             // Mostrando os comentários do post
             if(mysqli_num_rows($verificaTabelaComentarios) > 0){
                 $comentarios = mysqli_query($conexao, "SELECT * FROM comentarios WHERE post_id = ".$tabela["id"]. " ORDER BY id DESC");
                 while($comentario = mysqli_fetch_array($comentarios)){
+                    if($usuario_id == $comentario['usuario_id']){
+                        echo("<b>". $nome_usuario  ."</b> <br>");
+                    }
                     echo $comentario["comentario"] . "<br>";
+                    
+                    // Verificando se o comentário foi feito por aquele usuario e exibindo o botão de excluir comentário
+                    if($usuario_id == $comentario['usuario_id']){
+                        echo "<form action='../Banco de dados/mostrar-post-comentario-admin.php' method='POST'>
+                        <input type='submit' name='excluirComentario' value='Excluir Comentário'> <br> <br> <!--excluir comentario-->
+                        
+                        <input type='hidden' name='comentario' value='".$comentario["comentario"]."'>
+                        <input type='hidden' name='post_id' value='".$tabela["id"]."'>
+                        </form>";
+                    }
                 }
             } else{
                 echo "Sem comentários no momento.";
@@ -51,7 +78,7 @@
             <input type='submit' name='inserirComentario' value='Comentar'> <br> <br> <!--inserir novo comentario-->
             </form>
             ";
-
+            
             echo "<hr>";
         }
     } else{
@@ -61,7 +88,6 @@
     // Inserindo um novo comentário no banco de dados
     if (isset($_POST['inserirComentario'])) {
         $comentario = $_POST['comentario'];
-        $usuario_id = $_SESSION['usuario_id'];
         $post_id = $_POST['post_id'];
 
         if($comentario == ""){
@@ -76,15 +102,23 @@
         exit();
     }
 
-    // Excluindo um comentário do banco de dados (Melhorar depois)
-    // if(isset($_POST['excluirComentario'])){
-    //     $comentario = $_POST['comentario'];
-    //     $usuario_id = $_SESSION['usuario_id_admin'];
-    //     $post_id = $_POST['post_id'];
-
-    //     mysqli_query($conexao, "DELETE FROM comentarios WHERE comentario = '$comentario' AND usuario_id = '$usuario_id' AND post_id = '$post_id'");
+    // Excluindo um comentário do banco de dados
+    if(isset($_POST['excluirComentario'])){
+        $comentario = $_POST['comentario'];
+        $post_id = $_POST['post_id'];
         
-    //     header("Location: ../Paginas/index-admin.php");
-    //     exit();
-    // }
+
+        mysqli_query($conexao, "DELETE FROM comentarios WHERE comentario = '$comentario' AND post_id = '$post_id' AND usuario_id = '$usuario_id'");
+        
+        header("Location: ../Paginas/index.php");
+        exit();
+    }
+    // Excluindo um post do banco de dados
+    if(isset($_POST['excluirPost'])){
+        $post_id = $_POST['post_id'];
+
+        mysqli_query($conexao, "DELETE FROM posts WHERE id = '$post_id' AND usuario_id = '$usuario_id'");
+        header("Location: ../Paginas/index.php");
+        exit();
+    }
 ?>
